eb342b31160b382f9c40f6a9496b61c1c12f70ed
YvesPereira21 <yvespereira85@gmail.com>
2026-06-18 15:01:29 -0300

    build: setup markdown and syntax highlighting (prism)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/auth/auth.interceptor';
+import { provideMarkdown } from 'ngx-markdown'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors(
       [authInterceptor]
     )),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideMarkdown()
   ]
 };
